614dc2dea2cafcb1bb7ddf7de52b898bceeb8e27
Dilshanka Liyanage <34482402+dilthedev@users.noreply.github.com>
2019-10-19 18:41:00 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -11,13 +11,7 @@ Vue.use(VueTextareaAutosize);
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBWz9X9z1JYyTlc4lS2AGTqSuSPqjV96QY",
-  authDomain: "vcalander-32a6c.firebaseapp.com",
-  databaseURL: "https://vcalander-32a6c.firebaseio.com",
-  projectId: "vcalander-32a6c",
-  storageBucket: "vcalander-32a6c.appspot.com",
-  messagingSenderId: "70892137082",
-  appId: "1:70892137082:web:edc9ab776b3dd74e3569c3"
+  // ADD YOUR OWN FIREBASE APP DATA HERE
 });
 
 export const db = firebase.firestore();
